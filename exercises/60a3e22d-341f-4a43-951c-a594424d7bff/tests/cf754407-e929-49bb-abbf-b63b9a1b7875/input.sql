f4129e4d3dabb0c68670e7c2bebf58167225872a
José Carlos Paiva <jose.c.paiva@inesctec.pt>
2024-10-15 16:12:08 +0100

    created file exercises/60a3e22d-341f-4a43-951c-a594424d7bff/tests/cf754407-e929-49bb-abbf-b63b9a1b7875/input.sql

---
+++ exercises/60a3e22d-341f-4a43-951c-a594424d7bff/tests/cf754407-e929-49bb-abbf-b63b9a1b7875/input.sql
@@ -0,0 +1,5 @@
+INSERT INTO ciclista (ficha,fecha_nac,nombre,pais,anyo_retiro,anyo_prof)
+	VALUES (3,'1999-03-22','Juan','Italia',NULL,2020);
+INSERT INTO ciclista (ficha,fecha_nac,nombre,pais,anyo_retiro,anyo_prof)
+	VALUES (4,'1999-03-22','Pepe','Italia',NULL,2010);
+select * from ciclista order by ficha;
