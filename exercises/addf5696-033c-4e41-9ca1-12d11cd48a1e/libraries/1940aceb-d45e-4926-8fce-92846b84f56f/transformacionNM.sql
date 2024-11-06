7941bdfa85dc0b5f4b627868b52168d6b93fbd56
José Carlos Paiva <jose.c.paiva@inesctec.pt>
2024-11-06 11:05:52 +0000

    created file exercises/addf5696-033c-4e41-9ca1-12d11cd48a1e/libraries/1940aceb-d45e-4926-8fce-92846b84f56f/transformacionNM.sql

---
+++ exercises/addf5696-033c-4e41-9ca1-12d11cd48a1e/libraries/1940aceb-d45e-4926-8fce-92846b84f56f/transformacionNM.sql
@@ -0,0 +1,28 @@
+CREATE TABLE ciclista (
+	ficha integer NOT NULL,
+	fecha_nac date NOT NULL,
+	nombre varchar(50) NOT NULL,
+	pais varchar(30) NOT NULL DEFAULT 'ESPAÑA',
+	anyo_retiro integer NULL,
+	anyo_prof integer NOT NULL,
+	CONSTRAINT ciclista_pk PRIMARY KEY (ficha),
+	CONSTRAINT ciclista_check CHECK (anyo_prof >= 2015)
+);
+
+CREATE TABLE equipo (
+	nombre varchar(100) NOT NULL,
+	anyo_apar integer NOT NULL,
+	anyo_des integer NULL,
+	CONSTRAINT equipo_pk PRIMARY KEY (nombre)
+);
+
+CREATE TABLE pertenece (
+	ficha integer NOT NULL,
+	nombre varchar(100) NOT NULL,
+	anyo integer NOT NULL,
+	CONSTRAINT pertenece_pk PRIMARY KEY (ficha,nombre,anyo)
+);
+
+ALTER TABLE pertenece ADD CONSTRAINT pertenece_fk FOREIGN KEY (ficha) REFERENCES ciclista(ficha);
+ALTER TABLE pertenece ADD CONSTRAINT pertenece_fk_1 FOREIGN KEY (nombre) REFERENCES equipo(nombre);
+
