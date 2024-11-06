8515a5576a37963d923510a884ec9147baf2a63a
José Carlos Paiva <jose.c.paiva@inesctec.pt>
2024-11-06 11:06:46 +0000

    created file exercises/d2af0d00-9324-4e81-8aba-d9c02786a716/libraries/4ff1e1fa-c4bd-4b38-a783-2db60ff2157b/ejer3Library.sql

---
+++ exercises/d2af0d00-9324-4e81-8aba-d9c02786a716/libraries/4ff1e1fa-c4bd-4b38-a783-2db60ff2157b/ejer3Library.sql
@@ -0,0 +1,62 @@
+CREATE TABLE municipios (
+	cMunicipio  varchar(3) NOT NULL,
+	nombre  varchar(100) NOT NULL,
+	poblacion integer NOT NULL,
+	mesas integer NOT NULL,
+	censo integer NOT NULL,
+	vBlanco integer NOT NULL,
+	vNulos integer NOT NULL,
+	vCandidaturas integer NOT NULL
+);
+
+INSERT INTO municipios (cMunicipio,nombre,poblacion,mesas,censo,vBlanco,vNulos,vCandidaturas) VALUES
+	 ('001','Abanilla','6063','10','4538','9','25','3685'),
+	 ('002','Abarán','13030','17','9729','51','79','6425'),
+	 ('003','Águilas','35031','39','25072','86','124','14896'),
+	 ('004','Albudeite','1361','2','1129','3','7','1008'),
+	 ('005','Alcantarilla','41622','47','30194','92','122','18283'),
+	 ('006','Aledo','1022','1','831','2','4','758'),
+	 ('007','Alguazas','9525','11','6480','22','48','4347'),
+	 ('008','Alhama de Murcia','21657','23','14470','45','88','9282'),
+	 ('009','Archena','19053','20','12716','49','80','9376'),
+	 ('010','Beniel','11274','11','6859','21','41','5044');
+INSERT INTO municipios (cMunicipio,nombre,poblacion,mesas,censo,vBlanco,vNulos,vCandidaturas) VALUES
+	 ('011','Blanca','6564','7','4702','36','51','3555'),
+	 ('012','Bullas','11547','16','9066','41','55','6530'),
+	 ('013','Calasparra','10165','13','7461','32','49','5099'),
+	 ('014','Campos del Río','2045','3','1589','7','12','1217'),
+	 ('015','Caravaca de la Cruz','25730','31','19339','67','105','12736'),
+	 ('016','Cartagena','213943','259','152589','410','424','86529'),
+	 ('017','Cehegín','15022','20','11469','43','53','8221'),
+	 ('018','Ceutí','11565','13','8330','31','66','5962'),
+	 ('019','Cieza','34889','41','25853','112','145','14502'),
+	 ('020','Fortuna','10060','11','6448','25','50','5284');
+INSERT INTO municipios (cMunicipio,nombre,poblacion,mesas,censo,vBlanco,vNulos,vCandidaturas) VALUES
+	 ('021','Fuente Álamo de Murcia','16184','18','9437','34','43','6108'),
+	 ('022','Jumilla','25547','26','17069','83','117','10078'),
+	 ('023','Librilla','5196','6','3872','17','35','2918'),
+	 ('024','Lorca','93079','104','60494','203','256','38295'),
+	 ('025','Lorquí','7002','7','5005','18','38','3393'),
+	 ('026','Mazarrón','31562','32','16267','116','166','9905'),
+	 ('027','Molina de Segura','70964','82','49789','149','210','31115'),
+	 ('028','Moratalla','7954','10','6396','26','42','3942'),
+	 ('029','Mula','16790','19','12053','42','70','8394'),
+	 ('030','Murcia','447182','519','320347','914','1061','206050');
+INSERT INTO municipios (cMunicipio,nombre,poblacion,mesas,censo,vBlanco,vNulos,vCandidaturas) VALUES
+	 ('031','Ojós','500','1','506','0','3','463'),
+	 ('032','Pliego','3842','5','2857','17','28','2355'),
+	 ('033','Puerto Lumbreras','14981','16','10350','66','82','7380'),
+	 ('034','Ricote','1301','2','1073','5','16','862'),
+	 ('035','San Javier','31905','33','19279','76','69','11907'),
+	 ('036','San Pedro del Pinatar','25167','24','15290','51','95','9121'),
+	 ('037','Torre-Pacheco','35614','32','19270','66','90','12269'),
+	 ('038','Las Torres de Cotillas','21375','23','15674','28','63','9518'),
+	 ('039','Totana','31639','32','20232','104','116','12367'),
+	 ('040','Ulea','879','1','701','5','7','597');
+INSERT INTO municipios (cMunicipio,nombre,poblacion,mesas,censo,vBlanco,vNulos,vCandidaturas) VALUES
+	 ('041','La Unión','19907','23','13972','44','53','8430'),
+	 ('042','Villanueva del Río Segura','2758','4','2191','7','12','1627'),
+	 ('043','Yecla','34234','41','25390','131','223','17726'),
+	 ('901','Santomera','16105','17','10530','55','50','7379'),
+	 ('902','Los Alcázares','15674','15','8246','35','45','5156');
+
